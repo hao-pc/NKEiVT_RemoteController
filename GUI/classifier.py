0dfec15c1ce24aabe535a596f188a1554230c789
hao-pc <75453061+hao-pc@users.noreply.github.com>
2024-10-13 12:43:44 +0700

    Докручен -1 класс

--- GUI/classifier.py
+++ GUI/classifier.py
@@ -2,22 +2,35 @@ import keras
 import tensorflow as tf
 import pickle
 
+
 class classif:
     def __init__(self):
-        self.model = tf.keras.models.load_model(r"Models\command_classifire.h5")
-        with open(r'Models\tokenizer.pickle', 'rb') as handle:
+        self.model = tf.keras.models.load_model("Models/command_classifire.h5")
+        with open(r'Models/tokenizer.pickle', 'rb') as handle:
             self.tokenizer = pickle.load(handle)
-        with open(r'Models\attribute_dict.pickle', 'rb') as handle:
+        with open(r'Models/attribute_dict.pickle', 'rb') as handle:
             self.attribute_dict = pickle.load(handle)
+
     def predict(self, text):
         u_text = []
         attribute = -1
         u_text.append(text.split())
-        for i in u_text[0]:
-            if i in self.attribute_dict:
-                attribute = self.attribute_dict[i]
-            else:
-                continue
+
+        # Преобразуем текст в формат, который понимает модель
         x = self.tokenizer.texts_to_matrix(u_text)
-        label = self.model.predict(x).argmax()
-        return label, attribute
+
+        # Предсказание модели
+        label_probs = self.model.predict(x)
+        label_corr = label_probs.argmax()
+        label_x = label_probs[0][label_corr]
+
+        # Если вероятность предсказания меньше 0.85, вернуть -1
+        if label_x < 0.85:
+            label_corr = -1
+            attribute = -1
+        else:
+            for i in u_text[0]:
+                if i in self.attribute_dict:
+                    attribute = self.attribute_dict[i]
+
+        return label_corr, attribute
